e8ac1441247ef793cbcd56451b6be71bf4699dc7
Bogdan Kovalsky <55618988+Bogdan12-dev@users.noreply.github.com>
2024-10-01 18:06:03 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,3 +6,4 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 from test import routes
+https://docs.google.com/forms/d/e/1FAIpQLSdIwn_eaq0HGwqqUKbEG8iYznn24HhRjT0LG6zDdl4hlu2OPA/viewform?usp=sharing
